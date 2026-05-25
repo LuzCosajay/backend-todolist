a40858ee6aba3cdd13587f573887adb6951858b9
Luz Cosajay <luz.cosajay@galileo.edu>
2026-05-25 15:45:57 -0600

    Actividad 6

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
@@ -16,9 +17,10 @@ connectDB();
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -39,12 +41,10 @@ app.use('/users', usersRouter);
 app.use('/tasks', tasksRouter);
 app.use('/goals', goalsRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
